afb6f72ba299e2e0037141fe48f07f1ade5caaf3
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2026-07-20 23:16:01 +0530

    Time: 0 ms (100%), Space: 9.7 MB (63.92%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int solve(int i,int j, vector<vector<int>> &dp){
-        if(j==0 || j==i){
+    int solve(int i, int j, vector<vector<int>>& dp) {
+        if (j == 0 || j == i) {
             return 1;
         }
-        if(dp[i][j]!=-1){
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        return dp[i][j]=solve(i-1,j-1,dp)+solve(i-1,j,dp);
+        return dp[i][j] = solve(i - 1, j - 1, dp) + solve(i - 1, j, dp);
     }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
+        vector<vector<int>> dp(numRows);
 
-        for(int i=0;i<numRows;i++){
-             vector<vector<int>> dp(numRows,vector<int>(numRows,-1));
+        for (int i = 0; i < numRows; i++) {
 
-            vector<int> rows;
-            for(int j=0;j<=i;j++){
-                rows.push_back(solve(i,j,dp));
+            dp[i].resize(i+1);
+
+            for (int j = 0; j <= i; j++) {
+                if(j==i || j==0){
+                    dp[i][j]=1;
+                }else{
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
             }
-            ans.push_back(rows);
         }
-        return ans;
-        
+        return dp;
     }
 };
