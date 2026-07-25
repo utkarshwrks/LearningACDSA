621d70936140343c3b2df37f185903b870c936f6
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2026-07-26 00:14:20 +0530

    Time: 3 ms (15.04%), Space: 9.2 MB (13.4%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -3,10 +3,8 @@ public:
     int maxProduct(int n) {
         string demo=to_string(n);
         sort(demo.begin(),demo.end());
-        int maxi=0;
-        for(int i=1;i<demo.size();i++){
-              maxi=max(maxi,(demo[i-1]-'0')*(demo[i]-'0'));
-        }
+        int end=demo.size()-1;
+        int maxi=(demo[end]-'0')*(demo[end-1]-'0');
         return maxi;
     }
 };
