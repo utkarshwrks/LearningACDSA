6cecae59e3a5333a947d83a01d284a4402de8e1b
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2026-08-22 23:19:25 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.87%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+         long long low=1,high=num;
+
+        while(low<=high){
+            long long mid= low + (high - low) / 2;
+
+            if(mid*mid==num) return mid;
+             if (mid * mid < num)
+                low = mid + 1;
+            else
+                high = mid - 1;
+
+        }
+        return false;
+
+
+    }
+};
