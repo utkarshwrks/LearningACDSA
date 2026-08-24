508473589baa00104479cd9ec4bb844ba52ff1ff
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2026-08-25 01:27:05 +0530

    Time: 43 ms (77.86%), Space: 105.2 MB (36.26%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+
+        sort(nums.begin(), nums.end());
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            while (nums[right] - nums[left] > 2 * k) {
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
