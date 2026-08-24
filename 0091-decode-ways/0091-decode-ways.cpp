b81478d6410f804efda3e897d34aa6e324ffea05
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2026-08-24 23:26:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (79.81%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -20,11 +20,29 @@ public:
         }
         return dp[i] = ways;
     }
+    int solvetab(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '0') {
+                continue;
+            }
+            dp[i] = dp[i + 1];
+            if (i + 1 < n) {
+                int num = (s[i] - '0') * 10 + (s[i + 1] - '0');
+
+                if (num >= 10 && num <= 26) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
+    }
     int numDecodings(string s) {
         int n = s.size();
         vector<int> dp(n + 1, -1);
-        
-        return solve(0, s, dp);
-       
+
+        return solvetab( s );
     }
 };
