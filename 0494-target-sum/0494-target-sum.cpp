b63dd9058b775b006521abef76c9c407a0fcfb45
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2026-09-23 01:29:13 +0530

    Time: 3 ms (86.02%), Space: 15.6 MB (46.87%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    int solve(int i, int target, vector<int>& nums, vector<vector<int>>& dp) {
-        if (i == 0)
-            return target == 0;
-
-        if (dp[i][target] != -1)
-            return dp[i][target];
-
-        int notTake = solve(i - 1, target, nums, dp);
-
-        int take = 0;
-        if (nums[i - 1] <= target)
-            take = solve(i - 1, target - nums[i - 1], nums, dp);
-
-        return dp[i][target] = take + notTake;
-    }
-
     int findTargetSumWays(vector<int>& nums, int target) {
         int total = accumulate(nums.begin(), nums.end(), 0);
 
@@ -29,9 +13,20 @@ public:
 
         vector<vector<int>> dp(
             nums.size() + 1,
-            vector<int>(required + 1, -1)
+            vector<int>(required + 1, 0)
         );
 
-        return solve(nums.size(), required, nums, dp);
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= nums.size(); i++) {
+            for (int j = 0; j <= required; j++) {
+                dp[i][j] = dp[i - 1][j];
+
+                if (nums[i - 1] <= j)
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+            }
+        }
+
+        return dp[nums.size()][required];
     }
 };
